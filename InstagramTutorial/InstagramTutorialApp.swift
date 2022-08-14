7a97a1d8057676250d2996fc1aa5ef3c8459cc0f
Victor <victorroldan.dev@gmail.com>
2022-08-13 23:29:32 -0500

    Initial Commit

---
+++ InstagramTutorial/InstagramTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramTutorialApp.swift
+//  InstagramTutorial
+//
+//  Created by Victor Roldan on 13/08/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
